50fda88a9ba15eb580436df93751b530164e28de
tokitsu <toki2222son@yahoo.co.jp>
2021-07-14 13:32:45 +0900

    add hooks

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   post "checkout/create", to: "checkout#create", as:"checkout_create"
   post "billing_portal/create", to: "billing_portal#create", as: "billing_portal_create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :webhooks, only: [:create]
 end
